fb74b49fbfa4aeeae9d40fef495c997bb61d93f8
Papillon6814 <so1rou.k@gmail.com>
2020-10-13 21:38:23 +0900

    条件文

--- 2020/10/13/digits.cpp
+++ 2020/10/13/digits.cpp
@@ -19,13 +19,15 @@ int main() {
     ll n, ans = LL_INF;
     cin >> n;
 
-    for(ll i=1; i*i<n; i++) {
+    ll a, b;
+
+    for(ll i=1; i*i<=n; i++) {
         if (n%i == 0) {
-            ll a = (ll)n/i;
-            ll m = max(get_digit(i), get_digit(a));
-            if (m < ans) ans = m;
+            a = i;
+            b = n/i;
         }
     }
+    ans = max(get_digit(a), get_digit(b));
 
     cout << ans << endl;
 
