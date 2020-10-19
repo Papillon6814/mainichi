d23d293b06f8ae514f7d2f4ae8aa0dbfbe25c652
Papillon6814 <so1rou.k@gmail.com>
2020-10-19 19:06:34 +0900

    bit全探索のきれいな書き方練習

---
+++ 2020/10/19/train.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+// 例
+int main() {
+    string s;
+    cin >> s;
+
+    // 固定長のやつは固定で書いたほうがいいわ絶対
+    for(int bit=0; bit < (1<<3); bit++) {
+        int tmp = s.at(0) - '0';
+        string ans = "";
+        ans += s.at(0);
+        for(int i=0; i<3; i++) {
+            if(bit & (1<<i)) {
+                tmp += s.at(i+1) - '0';
+                ans += '+';
+                ans += s.at(i+1);
+            } else {
+                tmp -= s.at(i+1) - '0';
+                ans += '-';
+                ans += s.at(i+1);
+            }
+        }
+
+        if(tmp == 7) {
+            cout << ans << "=7";
+            break;
+        }
+    }
+
+    return 0;
+}
