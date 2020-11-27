841a0622c37d6e2f4bd1ef2d59d32813d837cc38
Papillon6814 <so1rou.k@gmail.com>
2020-11-28 00:53:03 +0900

    Create bfs.cpp

---
+++ 2020/11/28/bfs.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+int r, c;
+int sy, sx;
+int gy, gx;
+char m[51][51];
+int d[4][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
+int bfs(int sy, int sx) {
+    // distは歩数
+    vector<vector<int>> dist(r, vector<int>(c, INT_MAX));
+    queue<pii> q;
+
+    pii g(gy, gx);
+    // 最初の位置をキューに追加
+    q.push(pii(sy, sx));
+    // distのスタート地点の座標を0歩目として設定する
+    dist[sy][sx] = 0;
+
+    while(!q.empty()) {
+        pii t = q.front();
+        q.pop();
+        if(t == g) return dist[t.first][t.second];
+        for(int i=0; i<4; i++) {
+            int tx = t.second + d[i][0];
+            int ty = t.first + d[i][1];
+
+            if(tx<0 || ty<0 || ty>=r || m[ty][tx] == '#' || dist[ty][tx] <= dist[t.first][t.second] + 1) continue;
+            q.push(pii(ty, tx));
+            dist[ty][tx] = dist[t.first][t.second] + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    cin >> r >> c;
+    cin >> sy >> sx;
+    cin >> gy >> gx;
+
+    sy--; sx--; gy--; gx--;
+    for(int i=0; i<50; i++) cin >> m[i];
+
+    cout << bfs(sy, sx) << endl;
+
+    return 0;
+}
