f2ba3fe4e8125f5e87a7b660d1ff1ca183fb114d
Papillon6814 <so1rou.k@gmail.com>
2020-12-05 04:36:04 +0900

    幅優先探索

---
+++ 2020/12/05/bfs.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+int r, c;
+int sy, sx;
+int gy, gx;
+char m[51][51];
+int d[4][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
+int bfs(int sx, int sy) {
+    vector<vector<int>> dist(r, vector<int>(c, INT_MAX));
+    queue<pii> q;
+    pii g(gy, gx);
+    dist[sy][sx] = 0;
+
+    // キューに初期値であるsyとsxを代入する。
+    q.push(pii(sy, sx));
+    while(!q.empty()) {
+        pii t = q.front();
+        q.pop();
+
+        if (t == g) return dist[t.first][t.second];
+        for(int i=0; i<4; i++) {
+            int tx = t.second + d[i][0];
+            int ty = t.first + d[i][1];
+
+            if(tx < 0 || ty < 0 || ty >= r || m[ty][tx] == '#' || dist[ty][tx] <= dist[t.first][t.second] + 1) continue;
+
+            q.push(pii(ty, tx));
+            // 歩数の更新
+            dist[ty][tx] = dist[t.first][t.second] + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    cin >> r >> c;
+    cin >> sy >> sx;
+    cin >> gy >> gx;
+
+    sy--;sx--;gy--;gx--;
+    for(int i=0; i<r; i++) cin >> m[i];
+
+    cout << bfs(sx, sy) << endl;
+
+    return 0;
+}
