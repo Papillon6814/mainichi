6868a3319539a1e3c97215d32c9bdc44847b684e
Papillon6814 <so1rou.k@gmail.com>
2020-12-23 01:04:12 +0900

    Update osp.cpp

--- 2020/12/23/osp.cpp
+++ 2020/12/23/osp.cpp
@@ -7,17 +7,20 @@ vector<int> e[10];
 bool check[10];
 
 void dfs(int p, int tot) {
+    // 訪問済みに切り替える
     check[p] = true;
 
+    // すべてのノードを通ったらansに１加算
     if(tot == n) {
-        ans++
+        ans++;
     } else {
         for(int i=0; i<e[p].size(); i++) {
+            // ノードが訪問済みだったらcontinue
             if(check[e[p][i]]) continue;
             dfs(e[p][i], tot+1);
         }
     }
-
+    // 次の処理のために未訪問に戻す
     check[p] = false;
 }
 
@@ -26,6 +29,7 @@ int main() {
     for(int i=0; i<m; i++) {
         int x, y;
         cin >> x >> y;
+        // ノードを登録する
         e[x].push_back(y);
         e[y].push_back(x);
     }
