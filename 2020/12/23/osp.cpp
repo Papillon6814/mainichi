035797625ee2b184bdbe3b3331842f8c383cb4f4
Papillon6814 <so1rou.k@gmail.com>
2020-12-23 00:49:57 +0900

    dfs練習

---
+++ 2020/12/23/osp.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int ans, n, m;
+vector<int> e[10];
+bool check[10];
+
+void dfs(int p, int tot) {
+    check[p] = true;
+
+    if(tot == n) {
+        ans++
+    } else {
+        for(int i=0; i<e[p].size(); i++) {
+            if(check[e[p][i]]) continue;
+            dfs(e[p][i], tot+1);
+        }
+    }
+
+    check[p] = false;
+}
+
+int main() {
+    cin >> n >> m;
+    for(int i=0; i<m; i++) {
+        int x, y;
+        cin >> x >> y;
+        e[x].push_back(y);
+        e[y].push_back(x);
+    }
+    dfs(1, 1);
+
+    cout << ans << endl;
+
+    return 0;
+}
