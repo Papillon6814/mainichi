10cd103fdb6e5d70364d82fe78003cc185a746b3
Papillon6814 <so1rou.k@gmail.com>
2021-01-13 03:02:46 +0900

    区間スケジューリング

---
+++ 2021/01/13/island.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+vector<pii> v;
+
+int now = -1;
+int ans = 0;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for(int i=0; i<m; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        v.push_back(pii(b, a));
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i=0; i<v.size(); i++) {
+        int b = v[i].first;
+        int a = v[i].second;
+        if (a <= now && now < b) {
+            // do nothing
+        } else {
+            now = b-1;
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
