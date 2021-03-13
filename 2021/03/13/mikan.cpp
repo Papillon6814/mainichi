3edec9835b0da66f45e505234d74952e7897ee1f
Papillon6814 <so1rou.k@gmail.com>
2021-03-13 22:42:17 +0900

    shit atcoder

---
+++ 2021/03/13/mikan.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vc = vector<char>;
+using vii = vector<vector<int>>;
+using vll = vector<vector<ll>>;
+using vcc = vector<vector<char>>;
+using vs = vector<string>;
+using Pi = pair<int , int>;
+using Pl = pair<ll, ll>;
+using vpi = vector<Pi>;
+using vpl = vector<Pl>;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define REP(n) for (ll i = 0; i < n; i++)
+#define repint(i, n) for (int i = 0; i < n; i++)
+#define rep2(i, s, n) for (int i = (s); i < n; i++)
+#define REP2(s, n) for (int i = (s); i < n; i++)
+#define rep3(i ,j, n, m) rep(i, n)rep(j, m)
+#define REP3(n, m) rep(i, n)rep(j, m)
+#define sort(A) sort(A.begin(),A.end());
+#define reverse(A) reverse(A.begin(),A.end());
+#define k(s) cout << fixed << setprecision(s);
+const long double pi=3.14159265358979323846;
+const long long MOD=1000000007;
+
+int main() {
+    ll A, B, W;
+    cin >> A >> B >> W;
+
+    W *= 1000;
+    ll mi = (ll)W/B;
+    if (W-B*mi != 0) mi++;
+    ll ma = (ll)W/A;
+
+    string ans = to_string(mi) + " " + to_string(ma);
+    ll cnt = 0;
+    for(ll i=A; i<=B; i++) if(W%i == 0) cnt++;
+    if(cnt == B-A) ans = "UNSATISFIABLE";
+
+    cout << ans << endl;
+}
