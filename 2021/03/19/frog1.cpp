6139f26b4d9e5b3d70153393d1eba67a7b57ff5d
Papillon6814 <so1rou.k@gmail.com>
2021-03-19 00:45:50 +0900

    Create frog1.cpp

---
+++ 2021/03/19/frog1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vc = vector<char>;
+using vii = vector<vector<int>>;
+using vll = vector<vector<ll>>;
+using vcc = vector<vector<char>>;
+using vs = vector<string>;
+using Pi = pair<int , int>;
+using Pl = pair<ll, ll>;
+using vpi = vector<Pi>;
+using vpl = vector<Pl>;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define REP(n) for (ll i = 0; i < n; i++)
+#define repint(i, n) for (int i = 0; i < n; i++)
+#define rep2(i, s, n) for (int i = (s); i < n; i++)
+#define REP2(s, n) for (int i = (s); i < n; i++)
+#define rep3(i ,j, n, m) rep(i, n)rep(j, m)
+#define REP3(n, m) rep(i, n)rep(j, m)
+#define sort(A) sort(A.begin(),A.end());
+#define reverse(A) reverse(A.begin(),A.end());
+#define k(s) cout << fixed << setprecision(s);
+const long double pi=3.14159265358979323846;
+const long long MOD=1000000007;
+
+int main() {
+    ll N;
+    cin >> N;
+    ll h[100010];
+    ll dp[100010];
+    REP(N) {
+        cin >> h[i];
+        dp[i] = __INT_MAX__;
+    }
+    dp[0] = 0;
+
+    REP(N) {
+        dp[i+1] = min(dp[i+1], dp[i]+abs(h[i]-h[i+1]));
+        dp[i+2] = min(dp[i+2], dp[i]+abs(h[i]-h[i+2]));
+    }
+
+    cout << dp[N-1] << endl;
+}
