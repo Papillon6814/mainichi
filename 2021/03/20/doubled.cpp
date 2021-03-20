524086b889a84ecbfc01fc2d01b61509d6da732d
Papillon6814 <so1rou.k@gmail.com>
2021-03-20 22:35:00 +0900

    atcoder

---
+++ 2021/03/20/doubled.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vc = vector<char>;
+using vii = vector<vector<int>>;
+using vll = vector<vector<ll>>;
+using vcc = vector<vector<char>>;
+using vs = vector<string>;
+using Pi = pair<int , int>;
+using Pl = pair<ll, ll>;
+using vpi = vector<Pi>;
+using vpl = vector<Pl>;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define REP(n) for (ll i = 0; i < n; i++)
+#define repint(i, n) for (int i = 0; i < n; i++)
+#define rep2(i, s, n) for (int i = (s); i < n; i++)
+#define REP2(s, n) for (int i = (s); i < n; i++)
+#define rep3(i ,j, n, m) rep(i, n)rep(j, m)
+#define REP3(n, m) rep(i, n)rep(j, m)
+#define sort(A) sort(A.begin(),A.end());
+#define reverse(A) reverse(A.begin(),A.end());
+#define k(s) cout << fixed << setprecision(s);
+const long double pi=3.14159265358979323846;
+const long long MOD=1000000007;
+
+ll getDigit(ll num) {
+    ll digit = 0;
+    while(num != 0) {
+        num /= 10;
+        digit++;
+    }
+
+    return digit;
+}
+
+int main() {
+    ll N;
+    cin >> N;
+    bool isOdd;
+
+    ll halfDigit = getDigit(N)/2;
+    ll p = pow(10, halfDigit);
+    ll right = N % p;
+    ll left = 0;
+
+    if (getDigit(N)%2 == 1) {
+        isOdd = true;
+        left = N / p;
+        ll tmp = getDigit(left);
+        ll tmpp = pow(10, tmp-1);
+        left = tmpp - 1;
+    } else {
+        isOdd = false;
+        left = N / p;
+    }
+    
+    //cout << left << " " << right << endl;
+
+    if (left > right && !isOdd) {
+        cout << left-1 << endl;
+    } else  {
+        cout << left << endl;
+    }
+    //cout << left << endl;
+}
