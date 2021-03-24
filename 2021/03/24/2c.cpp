216b70a4e9acd17e4d1b7a62f7545884e3fd5a2d
Papillon6814 <so1rou.k@gmail.com>
2021-03-24 09:30:33 +0900

    ちょっと上達したかも

---
+++ 2021/03/24/2c.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vc = vector<char>;
+using vii = vector<vector<int>>;
+using vll = vector<vector<ll>>;
+using vcc = vector<vector<char>>;
+using vs = vector<string>;
+using Pi = pair<int , int>;
+using Pl = pair<ll, ll>;
+using vpi = vector<Pi>;
+using vpl = vector<Pl>;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define REP(n) for (ll i = 0; i < n; i++)
+#define repint(i, n) for (int i = 0; i < n; i++)
+#define rep2(i, s, n) for (int i = (s); i < n; i++)
+#define REP2(s, n) for (int i = (s); i < n; i++)
+#define rep3(i ,j, n, m) rep(i, n)rep(j, m)
+#define REP3(n, m) rep(i, n)rep(j, m)
+#define sort(A) sort(A.begin(),A.end());
+#define reverse(A) reverse(A.begin(),A.end());
+#define k(s) cout << fixed << setprecision(s);
+const long double pi=3.14159265358979323846;
+const long long MOD=1000000007;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    int t[2] = {0, 0};
+
+    REP(N) {
+        string s;
+        cin >> s;
+        int cc = 0;
+        rep(j, M) {
+            int c = s[j] - '0';
+            int cc ^= c;
+        }
+
+        t[cc]++;
+    }
+
+    cout << 1 * t[0] << t[1] << endl;
+}
